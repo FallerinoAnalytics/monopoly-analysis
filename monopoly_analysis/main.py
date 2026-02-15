67063a161ebdae6fa59e1d964120e28b0c680448
Luis F <luis.faller@live.de>
2026-02-15 23:07:44 +0100

    feat: transition matrix algorithm

--- monopoly_analysis/main.py
+++ monopoly_analysis/main.py
@@ -2,5 +2,6 @@ import probabilities
 
 if __name__ == "__main__":
     ini = probabilities.Probabilities()
-    probs = ini.get_probabilities_of_community_chest_cards(16, 2)
-    print(probs)
+    states = ini.create_state_space()
+    matrix = ini.create_transition_matrix(states)
+    print(len(matrix))
